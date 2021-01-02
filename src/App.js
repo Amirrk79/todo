171854dd94698ff6f30a1b46395b1caf0b7868f0
Amirrk79 <amirmntmntmntmnt@gmail.com>
2021-01-03 00:40:32 +0330

    Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Component/Header';
+import TaskBar from './Component/Taskbar';
+import TaskList from './Component/TaskList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div class='d-flex flex-column align-items-center'>
+        <Header />
+        <TaskBar />
+        <TaskList />
+      </div>
+    )
+  };
+
 
 export default App;
